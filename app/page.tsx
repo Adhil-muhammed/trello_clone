843cb39b72a6979e5079663dabc026dc46c3b26a
adhil muhammed <adhil.muhammed@codinghands.in>
2023-06-03 15:07:24 +0530

    set up-> header section and implement logo and search bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Header from '@/components/Header'
-import Image from 'next/image'
-
+import Header from "@/components/Header";
+// min-h-screen flex-col items-center justify-between p-24
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* Header componet */}
-      <Header/>
-
-
-      {/* Boared component */}
+    <main className="min-h-screen flex-col items-center justify-between">
+      <Header />
     </main>
-  )
+  );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,37 @@
-import Image from 'next/image'
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+// import "../app/globals.css";
+import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 
 function Header() {
   return (
-    <header className='bg-black w-1'>
-  <Image src='https://upload.wikimedia.org/wikipedia/en/8/8c/Trello_logo.svg' alt='image' width={100} height={50} className='w-44'  />
+    <header>
+      <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <Image
+          src="https://upload.wikimedia.org/wikipedia/en/8/8c/Trello_logo.svg"
+          alt="image"
+          width={100}
+          height={50}
+          className="w-40"
+        />
+
+        <div>
+          {/* search box  */}
+          <form className="flex h-12 flex-1  items-center space-x-5 bg-white rounded-md p-2 shadow-md md:flex-initial">
+            <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+            <input
+              type="text"
+              className="outline-none flex-1 p-2"
+              placeholder="Search"
+            />
+            <button hidden>search</button>
+          </form>
+
+          {/* avatar */}
+        </div>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
