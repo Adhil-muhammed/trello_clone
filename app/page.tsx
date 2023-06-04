44156c8160b1a9e82251687d06ea60368d4b4ad1
adhil muhammed <adhil.muhammed@codinghands.in>
2023-06-04 10:19:21 +0530

    style changed for new home page back ground

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
+
 // min-h-screen flex-col items-center justify-between p-24
 export default function Home() {
   return (
-    <main className="min-h-screen flex-col items-center justify-between">
+    <main>
       <Header />
+      <Board />
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,27 @@
+"use client";
 import React from "react";
 import Image from "next/image";
-// import "../app/globals.css";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
+import Avatar from "react-avatar";
 
 function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div
+          className="absolute left-0 right-0 w-full h-96 bg-gradient-to-br from-pink-400
+            to-[#0055D1] rounded-md blur-3xl filter opacity-50 -z-50 "
+        ></div>
+
         <Image
           src="https://upload.wikimedia.org/wikipedia/en/8/8c/Trello_logo.svg"
           alt="image"
           width={100}
           height={50}
-          className="w-40"
+          className="w-44"
         />
 
-        <div>
-          {/* search box  */}
+        <div className="flex items-center space-x-5 flex-1 justify-end w-full pt-10 md:pt-0">
           <form className="flex h-12 flex-1  items-center space-x-5 bg-white rounded-md p-2 shadow-md md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
             <input
@@ -27,8 +32,14 @@ function Header() {
             <button hidden>search</button>
           </form>
 
-          {/* avatar */}
+          <Avatar name="Adhil muhammed" round color="#0055D1" size="50" />
         </div>
+      </div>
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
+        <p className="flex items-center text-sm font-light font-serif shadow-xl rounded-xl w-fit bg-white italic text-[#0055D1] p-5">
+          <UserCircleIcon className="w-10 h-10 inline-block text-[#0055D1] mr-1" />
+          GPT is summarising your tasks for the day...
+        </p>
       </div>
     </header>
   );
