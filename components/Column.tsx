c50889553d1e2c8f09014481a71168e93caa4f58
adhil muhammed <adhil.muhammed@codinghands.in>
2023-06-13 23:36:45 +0530

    add board heading

--- components/Column.tsx
+++ components/Column.tsx
@@ -7,6 +7,14 @@ type Props = {
   index: number;
 };
 
+const idToColumnText: {
+  [key in TypedColumn]: string;
+} = {
+  done: "Done ",
+  inprogress: "In Progress",
+  todos: "To Do",
+};
+
 function Column({ id, index, todos }: Props) {
   return (
     <Draggable draggableId={id} index={index}>
@@ -18,17 +26,24 @@ function Column({ id, index, todos }: Props) {
         >
           {/* render dropable todos in column*/}
           <Droppable droppableId={index.toString()} type="card">
-            {(provided, snapshot) => (
-              <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className={`p-2 rounded-2xl shadow-md ${
-                  snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
-                }`}
-              >
-                <h2>{id}</h2>
-              </div>
-            )}
+            {(provided, snapshot) => {
+              return (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className={`p-2 rounded-2xl shadow-md ${
+                    snapshot.isDraggingOver ? "bg-green-200" : "bg-white/50"
+                  }`}
+                >
+                  <h2 className=" flex justify-between font-bold text-lg p-2">
+                    {idToColumnText[id]}
+                    <span className=" text-gray-500 bg-gray-200 rounded-full px-2 py-1 text-sm font-normal">
+                      {todos?.length}
+                    </span>
+                  </h2>
+                </div>
+              );
+            }}
           </Droppable>
         </div>
       )}
